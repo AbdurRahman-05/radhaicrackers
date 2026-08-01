0f68483d4d8d28628bd7192ed15372e62bd8374b
AbdurRahman-05 <massabdur123@gmail.com>
2026-08-01 21:42:19 +0530

    add prepareForValidation to SendOtpRequest to normalize phone input

--- app/Http/Requests/SendOtpRequest.php
+++ app/Http/Requests/SendOtpRequest.php
@@ -6,6 +6,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class SendOtpRequest extends FormRequest
 {
+    protected function prepareForValidation()
+    {
+        if ($this->has('phone')) {
+            $phone = preg_replace('/[^0-9]/', '', $this->phone);
+            if (strlen($phone) === 12 && str_starts_with($phone, '91')) {
+                $phone = substr($phone, 2);
+            } elseif (strlen($phone) === 11 && str_starts_with($phone, '0')) {
+                $phone = substr($phone, 1);
+            }
+            $this->merge([
+                'phone' => $phone,
+            ]);
+        }
+    }
+
     public function rules()
     {
         return [
